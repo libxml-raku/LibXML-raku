db93867f44e66e0f97df5823faa1fdc07a6e97b9
David Warring <david.warring@gmail.com>
2025-12-27 09:24:52 +1300

    Move, but don't change, XPath document() function

--- src/domXPath.c
+++ src/domXPath.c
@@ -22,122 +22,6 @@
 #include "xml6_ns.h"
 #include "xml6_ref.h"
 
-static void
-_documentFunction(xmlXPathParserContextPtr ctxt, int nargs) {
-    xmlXPathObjectPtr obj = NULL, obj2 = NULL;
-    xmlChar* base = NULL;
-    xmlChar* URI = NULL;
-
-    if ((nargs < 1) || (nargs > 2)) {
-        ctxt->error = XPATH_INVALID_ARITY;
-        return;
-    }
-    if (ctxt->value == NULL) {
-        ctxt->error = XPATH_INVALID_TYPE;
-        return;
-    }
-
-    if (nargs == 2) {
-        if (ctxt->value->type != XPATH_NODESET) {
-            ctxt->error = XPATH_INVALID_TYPE;
-            return;
-        }
-
-        obj2 = valuePop(ctxt);
-    }
-
-
-    /* first assure the LibXML error handler is deactivated
-       otherwise strange things might happen
-    */
-
-    if (ctxt->value->type == XPATH_NODESET) {
-        int i;
-        xmlXPathObjectPtr newobj, ret;
-
-        obj = valuePop(ctxt);
-        ret = xmlXPathNewNodeSet(NULL);
-
-        if (obj->nodesetval) {
-            for (i = 0; i < obj->nodesetval->nodeNr; i++) {
-                valuePush(ctxt,
-                          xmlXPathNewNodeSet(obj->nodesetval->nodeTab[i]));
-                xmlXPathStringFunction(ctxt, 1);
-                if (nargs == 2) {
-                    valuePush(ctxt, xmlXPathObjectCopy(obj2));
-                } else {
-                    valuePush(ctxt,
-                              xmlXPathNewNodeSet(obj->nodesetval->nodeTab[i]));
-                }
-                _documentFunction(ctxt, 2);
-                newobj = valuePop(ctxt);
-                ret->nodesetval = xmlXPathNodeSetMerge(ret->nodesetval,
-                                                       newobj->nodesetval);
-                newobj->nodesetval = NULL;
-                xmlXPathFreeObject(newobj);
-            }
-        }
-
-        xmlXPathFreeObject(obj);
-        if (obj2 != NULL)
-            xmlXPathFreeObject(obj2);
-        valuePush(ctxt, ret);
-
-        return;
-    }
-    /*
-     * Make sure it's converted to a string
-     */
-    xmlXPathStringFunction(ctxt, 1);
-    if (ctxt->value->type != XPATH_STRING) {
-        ctxt->error = XPATH_INVALID_TYPE;
-        if (obj2 != NULL)
-            xmlXPathFreeObject(obj2);
-
-        return;
-    }
-    obj = valuePop(ctxt);
-    if (obj->stringval == NULL) {
-        valuePush(ctxt, xmlXPathNewNodeSet(NULL));
-    } else {
-        if ((obj2 != NULL) && (obj2->nodesetval != NULL) &&
-            (obj2->nodesetval->nodeNr > 0)) {
-            xmlNodePtr target;
-
-            target = obj2->nodesetval->nodeTab[0];
-            if (target->type == XML_ATTRIBUTE_NODE) {
-                target = ((xmlAttrPtr) target)->parent;
-            }
-            base = xmlNodeGetBase(target->doc, target);
-        } else {
-            base = xmlNodeGetBase(ctxt->context->node->doc, ctxt->context->node);
-        }
-        URI = xmlBuildURI(obj->stringval, base);
-        if (base != NULL)
-            xmlFree(base);
-        if (URI == NULL) {
-            valuePush(ctxt, xmlXPathNewNodeSet(NULL));
-        } else {
-            if (xmlStrEqual(ctxt->context->node->doc->URL, URI)) {
-                valuePush(ctxt, xmlXPathNewNodeSet((xmlNodePtr)ctxt->context->node->doc));
-            }
-            else {
-                xmlDocPtr doc;
-                doc = xmlParseFile((const char *)URI);
-                if (doc == NULL)
-                    valuePush(ctxt, xmlXPathNewNodeSet(NULL));
-                else {
-                    valuePush(ctxt, xmlXPathNewNodeSet((xmlNodePtr) doc));
-                }
-            }
-            xmlFree(URI);
-        }
-    }
-    xmlXPathFreeObject(obj);
-    if (obj2 != NULL)
-        xmlXPathFreeObject(obj2);
-}
-
 static xmlNodePtr _domItemOwner(xmlNodePtr item) {
     xmlNodePtr owner = NULL;
     if (item != NULL) {
@@ -208,11 +92,11 @@ static xmlNodeSetPtr _domResizeNodeSet(xmlNodeSetPtr rv, int nodeMax) {
 
     size = nodeMax * sizeof(xmlNodePtr);
 
-    if (rv->nodeTab != NULL) {
-        temp = (xmlNodePtr *) xmlRealloc(rv->nodeTab, size);
+    if (rv->nodeTab == NULL) {
+        temp = (xmlNodePtr *) xmlMalloc(size);
     }
     else {
-        temp = (xmlNodePtr *) xmlMalloc(size);
+        temp = (xmlNodePtr *) xmlRealloc(rv->nodeTab, size);
     }
 
     assert(temp != NULL);
@@ -514,6 +398,122 @@ domXPathCtxtSetNode(xmlXPathContextPtr ctxt, xmlNodePtr node) {
     }
 
     return oldNode;
+}
+
+static void
+_documentFunction(xmlXPathParserContextPtr ctxt, int nargs) {
+    xmlXPathObjectPtr obj = NULL, obj2 = NULL;
+    xmlChar* base = NULL;
+    xmlChar* URI = NULL;
+
+    if ((nargs < 1) || (nargs > 2)) {
+        ctxt->error = XPATH_INVALID_ARITY;
+        return;
+    }
+    if (ctxt->value == NULL) {
+        ctxt->error = XPATH_INVALID_TYPE;
+        return;
+    }
+
+    if (nargs == 2) {
+        if (ctxt->value->type != XPATH_NODESET) {
+            ctxt->error = XPATH_INVALID_TYPE;
+            return;
+        }
+
+        obj2 = valuePop(ctxt);
+    }
+
+
+    /* first assure the LibXML error handler is deactivated
+       otherwise strange things might happen
+    */
+
+    if (ctxt->value->type == XPATH_NODESET) {
+        int i;
+        xmlXPathObjectPtr newobj, ret;
+
+        obj = valuePop(ctxt);
+        ret = xmlXPathNewNodeSet(NULL);
+
+        if (obj->nodesetval) {
+            for (i = 0; i < obj->nodesetval->nodeNr; i++) {
+                valuePush(ctxt,
+                          xmlXPathNewNodeSet(obj->nodesetval->nodeTab[i]));
+                xmlXPathStringFunction(ctxt, 1);
+                if (nargs == 2) {
+                    valuePush(ctxt, xmlXPathObjectCopy(obj2));
+                } else {
+                    valuePush(ctxt,
+                              xmlXPathNewNodeSet(obj->nodesetval->nodeTab[i]));
+                }
+                _documentFunction(ctxt, 2);
+                newobj = valuePop(ctxt);
+                ret->nodesetval = xmlXPathNodeSetMerge(ret->nodesetval,
+                                                       newobj->nodesetval);
+                newobj->nodesetval = NULL;
+                xmlXPathFreeObject(newobj);
+            }
+        }
+
+        xmlXPathFreeObject(obj);
+        if (obj2 != NULL)
+            xmlXPathFreeObject(obj2);
+        valuePush(ctxt, ret);
+
+        return;
+    }
+    /*
+     * Make sure it's converted to a string
+     */
+    xmlXPathStringFunction(ctxt, 1);
+    if (ctxt->value->type != XPATH_STRING) {
+        ctxt->error = XPATH_INVALID_TYPE;
+        if (obj2 != NULL)
+            xmlXPathFreeObject(obj2);
+
+        return;
+    }
+    obj = valuePop(ctxt);
+    if (obj->stringval == NULL) {
+        valuePush(ctxt, xmlXPathNewNodeSet(NULL));
+    } else {
+        if ((obj2 != NULL) && (obj2->nodesetval != NULL) &&
+            (obj2->nodesetval->nodeNr > 0)) {
+            xmlNodePtr target;
+
+            target = obj2->nodesetval->nodeTab[0];
+            if (target->type == XML_ATTRIBUTE_NODE) {
+                target = ((xmlAttrPtr) target)->parent;
+            }
+            base = xmlNodeGetBase(target->doc, target);
+        } else {
+            base = xmlNodeGetBase(ctxt->context->node->doc, ctxt->context->node);
+        }
+        URI = xmlBuildURI(obj->stringval, base);
+        if (base != NULL)
+            xmlFree(base);
+        if (URI == NULL) {
+            valuePush(ctxt, xmlXPathNewNodeSet(NULL));
+        } else {
+            if (xmlStrEqual(ctxt->context->node->doc->URL, URI)) {
+                valuePush(ctxt, xmlXPathNewNodeSet((xmlNodePtr)ctxt->context->node->doc));
+            }
+            else {
+                xmlDocPtr doc;
+                doc = xmlParseFile((const char *)URI);
+                if (doc == NULL)
+                    valuePush(ctxt, xmlXPathNewNodeSet(NULL));
+                else {
+                    valuePush(ctxt, xmlXPathNewNodeSet((xmlNodePtr) doc));
+                }
+            }
+            xmlFree(URI);
+        }
+    }
+    xmlXPathFreeObject(obj);
+    if (obj2 != NULL)
+        xmlXPathFreeObject(obj2);
 }
 
 DLLEXPORT xmlXPathContextPtr
